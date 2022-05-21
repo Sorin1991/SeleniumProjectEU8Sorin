5efb6bb7341ce4275639cd21c99f00b10dad5242
Sorin1991 <basco91@gmail.com>
2022-05-21 21:03:16 +0100

    T3

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -56,6 +56,52 @@ public class T3_CRM_LOGIN {
         BrowserUtils.verifyTitle(driver,"Portal");
 
     }
+    @Test
+    public void crm_login_test_2(){
+
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+
+        //3. Enter valid username
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@name='USER_LOGIN']"));
+        inputUsername.sendKeys("helpdesk1@cybertekschool.com");
+
+        //4. Enter valid password
+
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name='USER_PASSWORD']"));
+        inputPassword.sendKeys("UserUser");
+
+        //5. Click to `Log In` button
+        WebElement loginButton = driver.findElement(By.xpath("//input[@value='Log In']"));
+        loginButton.click();
+
+
+        //6. Verify title is as expected:
+        //Expected: Portal
+        BrowserUtils.verifyTitle(driver,"Portal");
+
+    }
+
+    @Test
+    public void crm_login_test_3(){
+
+        //2. Go to : https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        CRM_Utilities.crm_login(driver,"helpdesk2@cybertekschool.com","UserUser");
+        BrowserUtils.sleep(3);
+
+        BrowserUtils.verifyTitle(driver,"Portal");
+
+
+
+    }
+
+
+
+
+
 
     @AfterMethod
     public void tearDownMethod(){
