ca43bad5d152ae3e25c71dc4bfc70d4eb8926dbb
Sorin1991 <basco91@gmail.com>
2022-06-09 14:59:18 +0100

    TestBase

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T3_CRM_LOGIN.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day7_webtables_utilities_javafaker;
 
+import com.cydeo.tests.base.TestBase;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.CRM_Utilities;
 import com.cydeo.utilities.HandleWait;
@@ -13,19 +14,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class T3_CRM_LOGIN {
-    WebDriver driver;
-
-    @BeforeMethod
-    public void setUpMethod(){
-        //TC #3: Login scenario
-        //1. Create new test and make set ups
-        driver = WebDriverFactory.getDriver("Chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-
-    }
+public class T3_CRM_LOGIN extends TestBase {
 
     @Test
     public void crm_login_test(){
@@ -98,17 +87,6 @@ public class T3_CRM_LOGIN {
 
     }
 
-
-
-
-
-
-    @AfterMethod
-    public void tearDownMethod(){
-        HandleWait.sleep(4);
-
-        driver.close();
-    }
 
 
 }
